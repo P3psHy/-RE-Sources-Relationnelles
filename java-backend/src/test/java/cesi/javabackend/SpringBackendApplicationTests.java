dfb004df654e0a27f2ea8b69caec81f9aa45600e
antoine <antoine.bhr4@gmail.com>
2024-02-15 09:37:11 +0100

    feat: Implement Spring backend

---
+++ java-backend/src/test/java/cesi/javabackend/SpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cesi.javabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

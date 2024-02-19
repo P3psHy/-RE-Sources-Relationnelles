839c096d6c9c6b8ea12be2a1b41a67249bb1ff6d
antoine <antoine.bhr4@gmail.com>
2024-02-19 10:53:22 +0100

    feat: Create component for footer

---
+++ ressources-relationnelles/src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <ion-footer collapse="fade">
+        <ion-toolbar>
+            <ion-img class="logo" src="../sources/gouvernement-logos-idpqhzX_Mk.png"></ion-img>
+        </ion-toolbar>
+    </ion-footer>
+</template>
+
+<script setup>
+    import { IonFooter, IonToolbar, IonImg } from "@ionic/vue"
+</script>
+
+<style scoped>
+
+</style>

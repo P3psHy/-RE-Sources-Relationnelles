4519731c4de9e8281c647c8151f7577d3ffb0e99
P3psHy <76128319+P3psHy@users.noreply.github.com>
2024-02-19 16:22:50 +0100

    correction

--- java-backend/src/main/java/com/ajmservices/javabackend/model/RedigerMessage.java
+++ java-backend/src/main/java/com/ajmservices/javabackend/model/RedigerMessage.java
@@ -22,6 +22,14 @@ public class RedigerMessage {
 
     private String message;
 
+    public String getMessage(){
+        return message;
+    }
+
+    public void setMessage(String message){
+        this.message = message;
+    }
+
     // Getters and setters
 }
 

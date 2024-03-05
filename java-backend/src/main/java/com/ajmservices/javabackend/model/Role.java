8ac6f2f8a6d4e91861d872a9a0b9e64400d860da
antoine <antoine.bhr4@gmail.com>
2024-03-05 09:19:18 +0100

    feat: Add Role java model

---
+++ java-backend/src/main/java/com/ajmservices/javabackend/model/Role.java
@@ -0,0 +1,46 @@
+package com.ajmservices.javabackend.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idRole;
+
+    private String typeRole;
+
+    // Constructors
+    public Role() {
+    }
+
+    public Role(String typeRole) {
+        this.typeRole = typeRole;
+    }
+
+    // Getters and Setters
+    public int getIdRole() {
+        return idRole;
+    }
+
+    public void setIdRole(int idRole) {
+        this.idRole = idRole;
+    }
+
+    public String getTypeRole() {
+        return typeRole;
+    }
+
+    public void setTypeRole(String typeRole) {
+        this.typeRole = typeRole;
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "Role{" +
+                "idRole=" + idRole +
+                ", typeRole='" + typeRole +
+                '}';
+    }
+}

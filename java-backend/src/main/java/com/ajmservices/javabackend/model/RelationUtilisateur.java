c4503b10387000c08012e039799d0f5543aed1e1
P3psHy <76128319+P3psHy@users.noreply.github.com>
2024-03-05 14:10:07 +0100

    add relationUser

---
+++ java-backend/src/main/java/com/ajmservices/javabackend/model/RelationUtilisateur.java
@@ -0,0 +1,53 @@
+// package com.ajmservices.javabackend.model;
+
+// import jakarta.persistence.*;
+
+// @Entity
+// public class RelationUtilisateur {
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private int idRelationUtilisateur;
+
+//     private boolean estAccepte;
+
+//     @ManyToOne
+//     @JoinColumn(name = "id_utilisateur")
+//     private Utilisateur utilisateur1;
+
+//     private Utilisateur utilisateur2;
+
+    
+
+
+
+
+
+
+
+
+
+//     public Utilisateur getUtilisateur1() {
+//         return this.utilisateur1;
+//     }
+
+//     public void setUtilisateur1(Utilisateur utilisateur1) {
+//         this.utilisateur1 = utilisateur1;
+//     }
+
+//     public Utilisateur getUtilisateur2() {
+//         return this.utilisateur2;
+//     }
+
+//     public void setUtilisateur2(Utilisateur utilisateur2) {
+//         this.utilisateur2 = utilisateur2;
+//     }
+
+//     public boolean isEstAccepte() {
+//         return this.estAccepte;
+//     }
+
+//     public void setEstAccepte(boolean estAccepte) {
+//         this.estAccepte = estAccepte;
+//     }
+
+// }

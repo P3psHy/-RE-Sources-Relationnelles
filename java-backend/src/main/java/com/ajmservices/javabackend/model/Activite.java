127d608fc3a1c00b2aabba67bf254950934963f4
antoine <antoine.bhr4@gmail.com>
2024-03-05 14:54:33 +0100

    impr: Add Set of messages in Activite model

--- java-backend/src/main/java/com/ajmservices/javabackend/model/Activite.java
+++ java-backend/src/main/java/com/ajmservices/javabackend/model/Activite.java
@@ -1,9 +1,15 @@
 package com.ajmservices.javabackend.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import jakarta.persistence.*;
 
 @Entity
 public class Activite extends Ressource {
+
+    @OneToMany(mappedBy = "activite")
+    private Set<MessageActivite> messagesActivite = new HashSet<>();
 
     // Constructors
     public Activite() {
@@ -13,5 +19,13 @@ public class Activite extends Ressource {
     // public Activite(String nom) {
     //     super(nom);
     // }
+
+    public Set<MessageActivite> getMessagesActivite() {
+        return messagesActivite;
+    }
+
+    public void setMessagesActivite(Set<MessageActivite> messagesActivite) {
+        this.messagesActivite = messagesActivite;
+    }
 
 }

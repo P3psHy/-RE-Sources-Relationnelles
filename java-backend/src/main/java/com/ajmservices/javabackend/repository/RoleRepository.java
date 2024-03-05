c8808a25dfad0fb0a578f4cf9b0673c57f856820
antoine <antoine.bhr4@gmail.com>
2024-03-05 09:24:16 +0100

    feat: Add repositories for Categorie and Role

---
+++ java-backend/src/main/java/com/ajmservices/javabackend/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.ajmservices.javabackend.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import com.ajmservices.javabackend.model.Role;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface RoleRepository extends CrudRepository<Role, Integer> {
+
+}

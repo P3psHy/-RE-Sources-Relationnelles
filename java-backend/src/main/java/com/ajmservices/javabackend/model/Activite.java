5f3de067a241a15acbb64e0a02c0c0eea92fd24b
antoine <antoine.bhr4@gmail.com>
2024-03-05 11:06:28 +0100

    fix: Use inherit model for activity

--- java-backend/src/main/java/com/ajmservices/javabackend/model/Activite.java
+++ java-backend/src/main/java/com/ajmservices/javabackend/model/Activite.java
@@ -3,40 +3,15 @@ package com.ajmservices.javabackend.model;
 import jakarta.persistence.*;
 
 @Entity
-public class Activite {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int idType;
-
-    private String nom;
+public class Activite extends TypeRessource {
 
     // Constructors
     public Activite() {
+        super();
     }
 
     public Activite(String nom) {
-        this.nom = nom;
+        super(nom);
     }
 
-    // Getters and Setters
-    public int getIdType() {
-        return idType;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    // toString method
-    @Override
-    public String toString() {
-        return "Activite{" +
-                "idType=" + idType +
-                ", nom='" + nom +
-                '}';
-    }
 }

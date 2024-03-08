c49a69ee9b34fdf32084745c1067a4cebbaf5094
P3psHy <76128319+P3psHy@users.noreply.github.com>
2024-03-08 16:25:43 +0100

    correct afich data profil

--- ressources-relationnelles/src/components/ProfileData.vue
+++ ressources-relationnelles/src/components/ProfileData.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <IonContent>
       <div class="ion-padding">
           <h1>Mon profil</h1>
@@ -13,27 +13,71 @@
           <ion-button>Supprimer le compte</ion-button>
       </div>
   </IonContent>
+</template> -->
+
+<!-- <script setup lang="ts"> -->
+// import { IonContent } from '@ionic/vue';
+// import axios from 'axios';
+// import {reactive, onMounted} from 'vue';
+
+// const jsonData = reactive({
+//   nom: '',
+//   prenom: '',
+//   mail: ''
+// });
+
+// axios.get('jsondb/Utilisateurs.json')
+//     .then((response) => {
+//       console.log(response.data[0]);
+      
+//     });
+
+//     console.log(jsonData);  
+
+<!-- </script> -->
+
+
+<template>
+  <IonContent>
+    <div class="ion-padding">
+      <p v-if="premierUtilisateur">Nom: {{ premierUtilisateur.nom }}</p>
+      <p v-if="premierUtilisateur">Prénom: {{ premierUtilisateur.prenom }}</p>
+      <p v-if="premierUtilisateur">Email: {{ premierUtilisateur.mail }}</p>
+      <ion-button>Changer le mot de passe</ion-button>
+      <p></p>
+      <ion-button>Supprimer le compte</ion-button>
+    </div>
+  </IonContent>
 </template>
 
-<script setup lang="ts">
-import { IonContent } from '@ionic/vue';
+<script>
 import axios from 'axios';
-import {reactive, onMounted} from 'vue';
 
-const jsonData = reactive({
-  nom: '',
-  prenom: '',
-  mail: ''
-});
-
-axios.get('jsondb/Utilisateurs.json')
-    .then((response) => {
-      console.log(response.data[0]);
-      jsonData.nom = response.data[0].nom;
-      jsonData.prenom = response.data[0].prenom;
-      jsonData.mail = response.data[0].mail;
-    });
-
+export default {
+  data() {
+    return {
+      utilisateurs: []
+    };
+  },
+  computed: {
+    premierUtilisateur() {
+      return this.utilisateurs.length > 0 ? this.utilisateurs[0] : null;
+    }
+  },
+  mounted() {
+    this.fetchUtilisateurs();
+  },
+  methods: {
+    async fetchUtilisateurs() {
+      try {
+        const response = await axios.get('jsondb/Utilisateurs.json');
+        this.utilisateurs = response.data;
+      } catch (error) {
+        console.error('Erreur lors du chargement des utilisateurs:', error);
+      }
+    }
+  }
+};
 </script>
 
 <style scoped>

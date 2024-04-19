f9033c9a34a5aca64c714b9bdc34ef6acbdec6b7
antoine <antoine.bhr4@gmail.com>
2024-04-19 10:51:25 +0200

    feat: Add changes to display user data in profile page

--- ressources-relationnelles/src/components/ProfileData.vue
+++ ressources-relationnelles/src/components/ProfileData.vue
@@ -1,46 +1,55 @@
 <template>
   <IonContent>
-      <div class="ion-padding">
-          <h1>Mon profil</h1>
-          <label>Prénom</label>
-          <p>{{ jsonData.prenom}}</p>
-          <label>Nom</label>
-          <p>{{ jsonData.nom }}</p>
-          <label>Email</label>
-          <p>{{ jsonData.mail }}</p>
-          <ion-button>Changer le mot de passe</ion-button>
-          <p></p>
-          <ion-button>Supprimer le compte</ion-button>
-      </div>
+    <div class="ion-padding">
+        <p v-if="premierUtilisateur">Nom: {{ premierUtilisateur.nom }}</p>
+        <p v-if="premierUtilisateur">Prénom: {{ premierUtilisateur.prenom }}</p>
+        <p v-if="premierUtilisateur">Email: {{ premierUtilisateur.mail }}</p>
+        <ion-button>Changer le mot de passe</ion-button>
+        <p></p>
+        <ion-button>Supprimer le compte</ion-button>
+    </div>
   </IonContent>
 </template>
 
-<script setup lang="ts">
-import { IonContent } from '@ionic/vue';
-import axios from 'axios';
-import {reactive, onMounted} from 'vue';
+<script>
+  import axios from 'axios';
 
-const jsonData = reactive({
-  nom: '',
-  prenom: '',
-  mail: ''
-});
+  import { IonButton, IonContent } from '@ionic/vue';
+  import { defineComponent } from 'vue';
 
-axios.get('jsondb/Utilisateurs.json')
-    .then((response) => {
-      console.log(response.data[0]);
-      jsonData.nom = response.data[0].nom;
-      jsonData.prenom = response.data[0].prenom;
-      jsonData.mail = response.data[0].mail;
-    });
-
+  export default {
+    components: { IonButton, IonContent },
+    data() {
+      return {
+        utilisateurs: []
+      };
+    },
+    computed: {
+      premierUtilisateur() {
+        return this.utilisateurs.length > 0 ? this.utilisateurs[0] : null;
+      }
+    },
+    mounted() {
+      this.fetchUtilisateurs();
+    },
+    methods: {
+      async fetchUtilisateurs() {
+        try {
+          const response = await axios.get('jsondb/Utilisateurs.json');
+          this.utilisateurs = response.data;
+        } catch (error) {
+          console.error('Erreur lors du chargement des utilisateurs:', error);
+        }
+      }
+    }
+  };
 </script>
 
 <style scoped>
-.ion-padding {
-text-align: center;
-padding: 1rem;
-margin-left: auto;
-margin-right: auto;
-}
+  .ion-padding {
+      text-align: center;
+      padding: 1rem;
+      margin-left: auto;
+      margin-right: auto;
+  }
 </style>

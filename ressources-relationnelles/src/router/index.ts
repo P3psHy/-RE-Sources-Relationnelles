89d30d9aa049ac07e64222a3faf2621f5ff7173d
antoine <antoine.bhr4@gmail.com>
2024-05-24 09:45:31 +0200

    feat: Add listRessource and UserProfile components for Homepage

--- ressources-relationnelles/src/router/index.ts
+++ ressources-relationnelles/src/router/index.ts
@@ -13,83 +13,97 @@ import SubscriptionForm from '@/components/SubscriptionForm.vue';
 import RelationsPage from '@/views/RelationsPage.vue';
 import MessagingPage from '@/views/MessagingPage.vue';
 import Ressource from '@/components/Ressource.vue';
+import UserProfile from '@/components/UserProfile.vue';
+import ListRessources from '@/components/ListRessources.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    component: BaseLayout,
-    redirect: "home",
-    children: [
-      {
-        path: 'home',
-        name: 'Home',
-        component: HomePage
-      },
-      {
-        // la redirection se fait quand le / est absent au début du path
-        path: 'us',
-        name: 'Connection',
-        component: ConnectionPage,
-        redirect: "/us/connexion",
-        children: [
-          {
-            path: 'connexion',
-            name: 'Connexion',
-            component: ConnectionForm
-          },
-          {
-            path: 'inscription',
-            name: 'Inscription',
-            component: SubscriptionForm
-          }
-        ]
-      },
-      {
-        path: 'ressource',
-        name: 'Ressource',
-        component: RessourcePage,
-        children: [
-          {
-            path: 'new',
-            name: 'PublierRessource',
-            component: RessourceNew
-          },
-          {
-            path: 'see',
-            name: 'VoirRessource',
-            component: Ressource
-          },
-        ]
-      },
+	path: '/',
+	component: BaseLayout,
+	redirect: "home",
+	children: [
+	  {
+		path: 'home',
+		name: 'Home',
+		component: HomePage,
+		children: [
+		  {
+			path: 'user',
+			name: 'UserProfile',
+			component: UserProfile
+		  },
+		  {
+			path: 'ressources',
+			name: 'UserRessources',
+			component: ListRessources
+		  }
+		]
+	  },
+	  {
+		// la redirection se fait quand le / est absent au début du path
+		path: 'us',
+		name: 'Connection',
+		component: ConnectionPage,
+		redirect: "/us/connexion",
+		children: [
+		  {
+			path: 'connexion',
+			name: 'Connexion',
+			component: ConnectionForm
+		  },
+		  {
+			path: 'inscription',
+			name: 'Inscription',
+			component: SubscriptionForm
+		  }
+		]
+	  },
+	  {
+		path: 'ressource',
+		name: 'Ressource',
+		component: RessourcePage,
+		children: [
+		  {
+			path: 'new',
+			name: 'PublierRessource',
+			component: RessourceNew
+		  },
+		  {
+			path: 'see',
+			name: 'VoirRessource',
+			component: Ressource
+		  },
+		]
+	  },
 
-      {
-        path: 'relations',
-        name: 'Relations',
-        component: RelationsPage
-      },
-      {
-        path: 'messagerie',
-        name: 'Messagerie',
-        component: MessagingPage
-          
-      },
-      {
-        path: 'profil',
-        name: 'Profil',
-        component: ProfilePage
-          
-      },
-      {
-        path: 'privacy-policy',
-        name: 'PrivacyPolicy',
-        component: PrivacyPolicyPage
-      },
-      {
-        path: 'terms-conditions',
-        name: 'TermsConditions',
-        component: TermsConditionsPage
-      }
-    ]
+	  {
+		path: 'relations',
+		name: 'Relations',
+		component: RelationsPage
+	  },
+	  {
+		path: 'messagerie',
+		name: 'Messagerie',
+		component: MessagingPage
+		  
+	  },
+	  {
+		path: 'profil',
+		name: 'Profil',
+		component: ProfilePage
+		  
+	  },
+	  {
+		path: 'privacy-policy',
+		name: 'PrivacyPolicy',
+		component: PrivacyPolicyPage
+	  },
+	  {
+		path: 'terms-conditions',
+		name: 'TermsConditions',
+		component: TermsConditionsPage
+	  }
+	]
   },
 ]
 

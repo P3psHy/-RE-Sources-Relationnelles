9d29a78432cd4b4419e474b0fadda5aa9cc6480e
antoine <antoine.bhr4@gmail.com>
2024-06-07 15:57:20 +0200

    impr: Display ressources from db

--- ressources-relationnelles/src/components/Ressource.vue
+++ ressources-relationnelles/src/components/Ressource.vue
@@ -12,56 +12,36 @@
 </template>
 
 <script setup lang="ts">
-import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonImg, IonNavLink } from '@ionic/vue';
-import { computed, defineProps } from 'vue';
-import Vue from 'vue';
+  import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonImg, IonNavLink } from '@ionic/vue';
+  import { computed, defineProps } from 'vue';
+  import Vue from 'vue';
 
-const props = defineProps({
-  ressource: {
-    type: Object,
-    required: true
-  }
-});
+  const props = defineProps({
+    ressource: {
+      type: Object,
+      required: true
+    }
+  });
 
-// Limite de caractères pour la description
-const descriptionLimit = 100;
+  // Limite de caractères pour la description
+  const descriptionLimit = 100;
 
-// Computed property pour tronquer la description
-const truncatedDescription = computed(() => {
-  if (props.ressource.description.length > descriptionLimit) {
-    return props.ressource.description.slice(0, descriptionLimit) + '...';
-  }
-  return props.ressource.description;
-});
-// Computed property pour formater la date
-const formattedDate = computed(() => {
-  const date = new Date(props.ressource.datePublication);
-  const day = date.getDate().toString().padStart(2, '0');
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const year = date.getFullYear();
-  return `${day}/${month}/${year}`;
-});
+  // Computed property pour tronquer la description
+  const truncatedDescription = computed(() => {
+    if (props.ressource.description.length > descriptionLimit) {
+      return props.ressource.description.slice(0, descriptionLimit) + '...';
+    }
+    return props.ressource.description;
+  });
+  // Computed property pour formater la date
+  const formattedDate = computed(() => {
+    const date = new Date(props.ressource.datePublication);
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  });
 
-/* import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { onMounted, ref } from 'vue';
-import axios from "axios";
-
-const ressource = ref(null);
-
-const fetchData = async () => {
-  ressource.value = null;
-  try {
-    const response = await axios.get("jsondb/ressource.json");
-    ressource.value = response.data;
-    console.log(ressource.value)
-  } catch (error) {
-    console.error('Error fetching ressource:', error);
-  }
-};
-
-onMounted(() => {
-  fetchData();
-}); */
 </script>
 
 <style scoped>
